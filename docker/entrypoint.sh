923128611c1cdb87d3f9975b31ffc311a493fd11
Gbest05 <aladegbolahan28@gmail.com>
2026-08-15 03:05:14 +0100

    Update entrypoint runtime directories

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 PORT="${PORT:-80}"
 sed -i "s/LISTEN_PORT/${PORT}/g" /etc/nginx/nginx.conf
 
-# Ensure writable directories
+# Ensure runtime & log directories exist
+mkdir -p /run/nginx /var/log/nginx /var/log/supervisor /var/run
 mkdir -p /var/www/html/public/uploads/settings
 mkdir -p /var/www/html/database
 chmod -R 777 /var/www/html/public/uploads /var/www/html/database
